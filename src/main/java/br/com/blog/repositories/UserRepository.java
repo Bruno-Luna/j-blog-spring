732b60ccfdf0629d3e0a617dba7661cb1d30d66e
Bruno-Luna <luna.brsilva@gmail.com>
2024-02-24 12:43:59 -0300

    add userModel, UserController and UserRepository

---
+++ src/main/java/br/com/blog/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.blog.repositories;
+
+import br.com.blog.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
