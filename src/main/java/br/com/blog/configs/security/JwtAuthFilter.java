97c76f102dc261a9d80af4fe9bb8f5e0a97c5445
Bruno-Luna <luna.brsilva@gmail.com>
2026-09-12 10:44:44 -0300

    refactor JwtAuthFilter, UserDetailsServiceImpl, and UserResponseDTO to use jakarta packages and update field names

--- src/main/java/br/com/blog/configs/security/JwtAuthFilter.java
+++ src/main/java/br/com/blog/configs/security/JwtAuthFilter.java
@@ -8,7 +8,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
@@ -21,8 +24,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private UserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response, FilterChain chain)
-            throws javax.servlet.ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7);
--- src/main/java/br/com/blog/configs/security/UserDetailsServiceImpl.java
+++ src/main/java/br/com/blog/configs/security/UserDetailsServiceImpl.java
@@ -2,13 +2,13 @@ package br.com.blog.configs.security;
 
 import br.com.blog.models.UserModel;
 import br.com.blog.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 
 @Service
 @Transactional
